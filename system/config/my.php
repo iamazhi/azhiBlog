2730e5fcd8a4a76ada348370a247bdf4d175a2df
azhi <chencongzhi520@gmail.com>
2013-10-31 14:35:08 +0800

    * update blog to newest chanzhi.

--- system/config/my.php
+++ system/config/my.php
@@ -1,10 +1,10 @@
 <?php
-$config->installed    = true;	
-$config->debug        = true;	
-$config->requestType  = 'GET';	
-$config->db->host     = '127.0.0.1';	
-$config->db->port     = '3306';	
-$config->db->name     = 'chanzhi';	
-$config->db->user     = 'root';	
-$config->db->password = 'azhi@keda';		
-$config->db->prefix   = 'eps_';	
+$config->installed    = true;
+$config->debug        = true;
+$config->requestType  = 'PATH_INFO';
+$config->db->host     = '127.0.0.1';
+$config->db->port     = '3306';
+$config->db->name     = 'chanzhi';
+$config->db->user     = 'root';
+$config->db->password = 'zentao';
+$config->db->prefix   = 'eps_';
